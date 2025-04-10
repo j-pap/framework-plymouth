6aa8efd36bd79660ced9b8a82495afdb0c8c1166
j-pap <205946337+j-pap@users.noreply.github.com>
2025-04-09 22:07:21 -0500

    framework-plymouth: update with best practices

--- framework-plymouth.nix
+++ framework-plymouth.nix
@@ -6,8 +6,8 @@
 }:
 
 stdenvNoCC.mkDerivation {
-  name = "framework-plymouth";
-  version = "2021-12-20";
+  pname = "framework-plymouth";
+  version = "0-unstable-2021-12-20";
 
   src = fetchFromSourcehut {
     owner = "~jameskupke";
@@ -16,38 +16,47 @@ stdenvNoCC.mkDerivation {
     hash = "sha256-TuD+qHQ6+csK33oCYKfWRtpqH6AmYqvZkli0PtFm8+8=";
   };
 
+  sourceRoot = "source/framework";
   nativeBuildInputs = [
     imagemagick
   ];
 
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p "$out"/share/plymouth/themes
-    cp -r framework/ "$out"/share/plymouth/themes
-    for image in "$out"/share/plymouth/themes/framework/throbber*.png; do
+  buildPhase = ''
+    runHook preBuild
+    for image in throbber*.png; do
       [[ -e "$image" ]] || break
       magick "$image" -resize 25% "$image";
     done
+
     sed -i '{
       8s/3/2/     # decrease title font size from 30 to 20
       11s/382/8/  # move dialog to .8 of screen
       13s/382/3/  # move title to .3 of screen
       15s/5/8/    # move logo to .8 of screen
-      28s/^/UseFirmwareBackground=true\n/  # display FW image @ boot
-      31s/^/UseFirmwareBackground=true\n/  # display FW image @ shutdown
-      34s/^/UseFirmwareBackground=true\n/  # display FW image @ reboot
-    }' "$out"/share/plymouth/themes/framework/framework.plymouth
-    sed -i "s@/usr/@$out/@" "$out"/share/plymouth/themes/framework/framework.plymouth
+      28s/^/UseFirmwareBackground=true\n/  # display uefi logo @ boot
+      31s/^/UseFirmwareBackground=true\n/  # display uefi logo @ shutdown
+      34s/^/UseFirmwareBackground=true\n/  # display uefi logo @ reboot
+    }' framework.plymouth
+    runHook postBuild
+  '';
 
+  installPhase = ''
+    runHook preInstall
+    mkdir -p "$out"/share/plymouth/themes/framework
+    cp * "$out"/share/plymouth/themes/framework
+    sed -i "s@/usr/@$out/@" "$out"/share/plymouth/themes/framework/framework.plymouth
     runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Plymouth boot theme with animated Framework logo";
+    longDescription = ''
+      Framework's gear icon spins near the bottom of the screen, while also
+      enabling their UEFI firmware logo to be displayed in the middle.
+    '';
     homepage = "https://git.sr.ht/~jameskupke/framework-plymouth-theme";
-    license = licenses.mit;
+    license = lib.licenses.mit;
     maintainers = [ "j-pap" ];
-    platforms = platforms.linux;
+    platforms = lib.platforms.linux;
   };
 }
